5384038a2f5669104864f7972cf8966162de339b
BinaliBhakhar <binu.bhakhar@gmail.com>
2021-05-17 09:12:33 +0530

    initial commit

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -12,65 +12,66 @@ export class CategoriesComponent implements OnInit {
 
   ngOnInit(): void {
     //first: 
-var slideIndex = 1;         
-showDivs(slideIndex);
+// var slideIndex = 1;         
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-showDivs(slideIndex += n);
-}
+// function plusDivs(n) {
+// showDivs(slideIndex += n);
+// }
 
-function showDivs(n) {
-var i;
-var x = document.getElementsByClassName("slide1");
-if (n > x.length) {slideIndex = 1}    
-if (n < 1) {slideIndex = x.length}
-for (i = 0; i < x.length; i++) {
-//  x[i].style.display = "none";  
-}
-// x[slideIndex-1].style.display = "block";  
-}
+// function showDivs(n) {
+// var i;
+// var x = document.getElementsByClassName("slide1");
+// if (n > x.length) {slideIndex = 1}    
+// if (n < 1) {slideIndex = x.length}
+// for (i = 0; i < x.length; i++) {
+// //  x[i].style.display = "none";  
+// }
+// // x[slideIndex-1].style.display = "block";  
+// }
 
 //--------------------------------------------------------------------
                            //second: 
-var slideIndex2 = 1;         
-showDivs2(slideIndex2);
+// var slideIndex2 = 1;         
+// showDivs2(slideIndex2);
 
-function plusDivs2(n) {
-showDivs2(slideIndex2 += n);
-}
+// function plusDivs2(n) {
+// showDivs2(slideIndex2 += n);
+// }
 
-function showDivs2(n) {
-var i;
-var x = document.getElementsByClassName("slide2");
-if (n > x.length) {slideIndex2 = 1}    
-if (n < 1) {slideIndex2 = x.length}
-for (i = 0; i < x.length; i++) {
-//  x[i].style.display = "none";  
-}
-// x[slideIndex2-1].style.display = "block";  
-}
+// function showDivs2(n) {
+// var i;
+// var x = document.getElementsByClassName("slide2");
+// if (n > x.length) {slideIndex2 = 1}    
+// if (n < 1) {slideIndex2 = x.length}
+// for (i = 0; i < x.length; i++) {
+// //  x[i].style.display = "none";  
+// }
+// // x[slideIndex2-1].style.display = "block";  
+// }
 //--------------------------------------------------------------------
                           //third: using w3.js library
 // const myslide = w3.slideshow(".slide3",0)
 //--------------------------------------------------------------------
 
-var slideIndex3 = 1;              //fourth
-showDivs3(slideIndex3);
+// var slideIndex3 = 1;              //fourth
+// showDivs3(slideIndex3);
 
-function plusDivs3(n) {
-showDivs3(slideIndex3 += n);
-}
+// function plusDivs3(n) {
+// showDivs3(slideIndex3 += n);
+// }
 
-function showDivs3(n) {
-var i;
-var x = document.getElementsByClassName("slide4");
-if (n > x.length) {slideIndex3 = 1}    
-if (n < 1) {slideIndex3 = x.length}
-for (i = 0; i < x.length; i++) {
-//  x[i].style.display = "none";  
-}
-// x[slideIndex3-1].style.display = "block";  
-}
+// function showDivs3(n) {
+// var i;
+// var x = document.getElementsByClassName("slide4");
+// if (n > x.length) {slideIndex3 = 1}    
+// if (n < 1) {slideIndex3 = x.length}
+// for (i = 0; i < x.length; i++) {
+  
+// //  x[i].style.display = "none";  
+// }
+// // x[slideIndex3-1].style.display = "block";  
+// }
 
 
 //====================================================================
